0088c9682da70be38538f7bc72872d45f3fff958
김혜성 <khs990419@gmail.com>
2024-04-27 11:44:13 +0900

    [fix] remove dynamic color
    android.content.res.Resources$NotFoundException: Could not resolve resource value: 0x1060060.
    dynamicDarkColorScheme, dyanmicLightColorScheme 사용하지 않도록 수정

--- presentation/src/main/java/com/daejol/presentation/theme/Theme.kt
+++ presentation/src/main/java/com/daejol/presentation/theme/Theme.kt
@@ -44,14 +44,7 @@ fun CatdogcupTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
