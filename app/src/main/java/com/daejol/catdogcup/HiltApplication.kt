2bc65e12b9fff5445d2b62e215d4b07e3dd64b4c
betterafter <koin12000@naver.com>
2024-05-01 19:04:13 +0900

    [feature] Hilt 추가

---
+++ app/src/main/java/com/daejol/catdogcup/HiltApplication.kt
@@ -0,0 +1,11 @@
+package com.daejol.catdogcup
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ data/src/main/java/com/daejol/data/HiltApplication.kt
@@ -0,0 +1,11 @@
+package com.daejol.data
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
