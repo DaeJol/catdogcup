aa8fb1fe9940029989056190005d61975a39353c
keykat <koin12000@naver.com>
2024-06-13 02:19:42 +0900

    [feature] 성격 매칭 화면 추가

---
+++ presentation/src/main/java/com/daejol/presentation/personalMatchScreen/PersonalActivity.kt
@@ -0,0 +1,22 @@
+package com.daejol.presentation.personalMatchScreen
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class PersonalActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PersonalScreen().PersonalWidget()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        PersonalScreen().PersonalWidget()
+    }
+}
