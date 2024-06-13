537125ecbfb8014f3ec5d348c17149dcba8baafc
keykat <koin12000@naver.com>
2024-06-14 03:42:21 +0900

    [chores] merge conflict 해결

--- presentation/src/main/java/com/daejol/presentation/theme/Type.kt
+++ presentation/src/main/java/com/daejol/presentation/theme/Type.kt
@@ -1,16 +1,12 @@
 package com.daejol.presentation.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.daejol.presentation.R
-
-
 import com.daejol.presentation.R
 
 val oaGothic = FontFamily(
