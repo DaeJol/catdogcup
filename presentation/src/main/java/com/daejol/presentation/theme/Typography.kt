20cb9e609cb49d99e8485250fba72704a39c3fb7
keykat <koin12000@naver.com>
2024-06-17 15:04:54 +0900

    [refactor] Typography.kt 추가 후 Text.kt 변경, Type.kt 제거

--- presentation/src/main/java/com/daejol/presentation/theme/Typography.kt
+++ presentation/src/main/java/com/daejol/presentation/theme/Typography.kt
@@ -9,6 +9,37 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.daejol.presentation.R
 
+
+val Pretendard = FontFamily(
+    fonts = listOf(
+        Font(R.font.pretendard_light, weight = FontWeight.Light),
+        Font(R.font.pretendard_medium, weight = FontWeight.Medium),
+        Font(R.font.pretendard_black, weight = FontWeight.Black),
+        Font(R.font.pretendard_bold, weight = FontWeight.Bold),
+        Font(R.font.pretendard_extrabold, weight = FontWeight.ExtraBold),
+        Font(R.font.pretendard_extralight, weight = FontWeight.ExtraLight),
+        Font(R.font.pretendard_semibold, weight = FontWeight.SemiBold),
+        Font(R.font.pretendard_regular, weight = FontWeight.Normal),
+        Font(R.font.pretendard_thin, weight = FontWeight.Thin)
+    )
+)
+
+val MoveSans = FontFamily(
+    fonts = listOf(
+        Font(R.font.movesans_light),
+        Font(R.font.movesans_medium),
+        Font(R.font.movesans_bold),
+    )
+)
+
+val Gimpo = FontFamily(
+    fonts = listOf(
+        Font(R.font.gimpo_1, weight = FontWeight.Light),
+        Font(R.font.gimpo_2, weight = FontWeight.Normal)
+    )
+)
+
+
 val oaGothic = FontFamily(
     Font(R.font.oa_gothic_medium, FontWeight.Medium, FontStyle.Normal),
     Font(R.font.oa_gothic_extra_bold, FontWeight.ExtraBold, FontStyle.Normal)
@@ -51,5 +82,3 @@ val Typography = Typography(
     )
     */
 )
-
-
