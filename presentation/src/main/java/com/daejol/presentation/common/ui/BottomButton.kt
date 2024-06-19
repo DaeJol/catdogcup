f3b6aa80ee13df7f73409af16ae723c6a15cf837
keykat <koin12000@naver.com>
2024-06-19 16:27:10 +0900

    [feature] 바텀 버튼 공통화 모듈 개발

--- presentation/src/main/java/com/daejol/presentation/common/ui/BottomButton.kt
+++ presentation/src/main/java/com/daejol/presentation/common/ui/BottomButton.kt
@@ -1,28 +1,24 @@
-package com.daejol.presentation.common
+package com.daejol.presentation.common.ui
+
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daejol.presentation.ui.theme.CustomTextStyle
 import com.daejol.presentation.ui.theme.Orange100
 import com.daejol.presentation.ui.theme.Pretendard
 import com.daejol.presentation.ui.theme.White100
 
-@Preview
 @Composable
 fun BottomButton(onClick: () -> Unit) {
     return Button(
