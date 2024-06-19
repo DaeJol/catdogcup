1b2ef6a494bc7134776cc9b5114ca6b32710c467
keykat <koin12000@naver.com>
2024-06-19 16:27:25 +0900

    [feature] 공통화 모듈 테스트 중

---
+++ presentation/src/main/java/com/daejol/presentation/common/data/Extension.kt
@@ -0,0 +1,8 @@
+package com.daejol.presentation.common.data
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+
+//inline val Dp.px: Float
+//    get() = with(LocalDensity.current) { this@px.toPx() }
