4af7432c575a48cab531bfbb98c0e99d8a76738c
keykat <koin12000@naver.com>
2024-06-21 02:56:00 +0900

    [feature] 디자인 세부 정의

--- presentation/src/main/java/com/daejol/presentation/worldcup/WorldCupScreen.kt
+++ presentation/src/main/java/com/daejol/presentation/worldcup/WorldCupScreen.kt
@@ -1,18 +1,22 @@
 package com.daejol.presentation.worldcup
 
-import TopSectionWidget
+import com.daejol.presentation.worldcup.top.TopSectionWidget
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.daejol.presentation.common.ui.BottomButton
+import com.daejol.presentation.worldcup.middle.MiddleSectionWidget
 
 enum class WorldCupType {
     CAT, DOG, COMBINED
@@ -29,6 +33,7 @@ class WorldCupPreviewParameterProvider : PreviewParameterProvider<WorldCupType> 
 @Preview
 @Composable
 fun WorldCupScreen(
+    viewModel: WorldCupViewModel = viewModel(),
     @PreviewParameter(WorldCupPreviewParameterProvider::class) type: WorldCupType
 ) {
     val t = WorldCupType.DOG
@@ -40,19 +45,21 @@ fun WorldCupScreen(
             .fillMaxHeight(),
     ) {
         Box(
-            modifier = Modifier.weight(1F),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp),
             contentAlignment = Alignment.Center,
         ) {
             TopSectionWidget(t)
         }
         Box(
-            modifier = Modifier.weight(1F),
-//            contentAlignment = Alignment.TopStart,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(4F),
         ) {
-            MiddleSectionWidget(t)
+            MiddleSectionWidget(viewModel, t)
         }
         Box(
-            modifier = Modifier.weight(1F),
             contentAlignment = Alignment.BottomCenter,
         ) {
             BottomButton({})
