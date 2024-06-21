a33ab74f2e60896f71b904ce063ab2a8926ea336
Hyesung82 <khs990419@gmail.com>
2024-06-21 17:07:18 +0900

    [featrue] 홈 화면 앱바 수정

--- presentation/src/main/java/com/daejol/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/daejol/presentation/home/HomeScreen.kt
@@ -4,25 +4,31 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daejol.presentation.R
-import com.daejol.presentation.ui.theme.Blue10
-import com.daejol.presentation.ui.theme.Red100
+import com.daejol.presentation.ui.theme.Orange80
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,19 +37,31 @@ fun HomeScreen() {
     Scaffold(
         topBar = {
             TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Red100,
-                    titleContentColor = Blue10
-                ),
                 title = {
                     Row(
-                        verticalAlignment = Alignment.CenterVertically
+                        verticalAlignment = Alignment.CenterVertically,
                     ) {
-                        // TODO: coil 라이브러리 사용
                         Image(
                             painter = painterResource(id = R.drawable.catdogcup_logo),
-                            contentDescription = null,
-                            modifier = Modifier.width(36.dp)
+                            contentDescription = stringResource(R.string.home_app_bar_title),
+                            colorFilter = ColorFilter.tint(Orange80),
+                            modifier = Modifier
+                                .width(64.dp)
+                                .fillMaxHeight()
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* TODO: 품종 검색 */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = stringResource(R.string.home_app_bar_search)
+                        )
+                    }
+                    IconButton(onClick = { /* TODO: 마이페이지 */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = stringResource(R.string.home_app_bar_my_page)
                         )
                     }
                 }
