fd40143e88314a2e6bfb2a1f374b7629e5587031
keykat <koin12000@naver.com>
2024-09-01 12:36:37 +0900

    [feature] 타이머 위젯 추가

--- presentation/src/main/java/com/daejol/presentation/ui/story/StoryScreen.kt
+++ presentation/src/main/java/com/daejol/presentation/ui/story/StoryScreen.kt
@@ -4,23 +4,34 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.daejol.presentation.ui.theme.Black100
+import com.daejol.presentation.ui.theme.Orange100
+import com.daejol.presentation.ui.theme.Orange60
+import com.daejol.presentation.ui.theme.Orange80
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -56,10 +67,16 @@ fun StoryScreen(
 
     Column(
         modifier = Modifier
-            .background(Black100)
+            .fillMaxHeight()
+            .background(Black100),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        TimerWidget()
         HorizontalPager(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .width(imageWidth)
+                .height(imageHeight),
             verticalAlignment = Alignment.CenterVertically,
             state = pageState
         ) { page ->
@@ -78,7 +95,73 @@ fun StoryScreen(
 
 @Composable
 fun TimerWidget() {
-    Row {
-        
+    var ticks: MutableState<Long> = remember { mutableStateOf(0L) }
+    val timeRange = 10L
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            if (ticks.value >= 100 * 10 * 10) {
+                ticks.value = 0
+            }
+
+            delay(timeRange)
+            ticks.value += timeRange
+        }
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        TimerWidgetItem(color = Orange60, ticks = ticks, min = 0.0, max = 3333.0)
+        TimerWidgetItem(color = Orange80, ticks = ticks, min = 3333.0, max = 6666.0)
+        TimerWidgetItem(color = Orange100, ticks = ticks, min = 6666.0, max = 9999.0)
+    }
+}
+
+@Composable
+fun TimerWidgetItem(
+    color: Color,
+    ticks: MutableState<Long>,
+    min: Double,
+    max: Double,
+) {
+    val screenWidth = LocalConfiguration.current.screenWidthDp * 0.3
+    val rateWidth = if (ticks.value < min) {
+        0.toDouble()
+    } else if (ticks.value / max < 1) {
+        (ticks.value - min) / (max - min)
+    } else {
+        1.0
+    }
+
+    val barWidth = (screenWidth * rateWidth).dp
+
+    Box(
+        modifier = Modifier
+            .width(screenWidth.dp)
+            .height(10.dp),
+        contentAlignment = Alignment.TopStart
+    ) {
+//        Text(text = "$ticks")
+        Box(
+            contentAlignment = Alignment.TopStart,
+            modifier = Modifier
+                .background(color)
+                .width((barWidth))
+                .height(10.dp)
+        ) {
+
+        }
+        Box(
+            contentAlignment = Alignment.TopStart,
+            modifier = Modifier
+                .background(Color.Transparent)
+                .width(screenWidth.dp)
+                .height(10.dp)
+        ) {
+
+        }
     }
 }
