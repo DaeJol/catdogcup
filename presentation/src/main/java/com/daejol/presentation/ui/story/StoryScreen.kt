f2419f81862eac2d7e46f08c066850393e9fab4b
keykat <koin12000@naver.com>
2024-09-01 11:09:37 +0900

    [feature] story일 때는 systembar 색 검게

--- presentation/src/main/java/com/daejol/presentation/ui/story/StoryScreen.kt
+++ presentation/src/main/java/com/daejol/presentation/ui/story/StoryScreen.kt
@@ -1,6 +1,9 @@
 package com.daejol.presentation.ui.story
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,18 +12,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.daejol.presentation.ui.worldcup.WorldCupViewModel
+import com.daejol.presentation.ui.theme.Black100
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -28,17 +30,34 @@ fun StoryScreen(
     viewModel: StoryViewModel,
 ) {
     val imageWidth = LocalConfiguration.current.screenWidthDp.dp
-    val imageHeight = LocalConfiguration.current.screenHeightDp.dp
+    val imageHeight = (LocalConfiguration.current.screenHeightDp * 0.6).dp
     val images = viewModel.storyImages.value
-
-    println("[keykat] images: $images")
-    println("[keykat] count::: ${images.size}")
 
     val pageState = rememberPagerState {
         images.size
     }
 
-    Column {
+    LaunchedEffect(key1 = images) {
+        launch {
+            delay(3000)
+            with(pageState) {
+                val target = if (currentPage < pageCount - 1) currentPage + 1 else 0
+
+                animateScrollToPage(
+                    page = target,
+                    animationSpec = tween(
+                        durationMillis = 500,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .background(Black100)
+    ) {
         HorizontalPager(
             modifier = Modifier.fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically,
@@ -47,6 +66,7 @@ fun StoryScreen(
             AsyncImage(
                 model = images[page].imageRequest,
                 contentDescription = "",
+                contentScale = ContentScale.FillHeight,
                 modifier = Modifier
                     .width(imageWidth)
                     .height(imageHeight)
